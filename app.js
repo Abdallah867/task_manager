e20c39a0c4ac0a985d63c2856880f5f6e46383c0
Abdallah867 <abdoallahusma5704@gmail.com>
2024-06-18 00:43:50 +0200

    connecting to mongo db

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
-const express = require('express')
+const express = require("express");
+const connectDB = require("./db/connection");
 const app = express();
-const tasksRouter = require('./routes/tasks')
+const tasksRouter = require("./routes/tasks");
+require("dotenv").config();
 
 const port = 5000;
+const run = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => console.log(`http://localhost:${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+app.use(express.json());
 
-app.use(express.json())
+app.use("/api/v1/tasks", tasksRouter);
 
-app.use('/api/v1/tasks', tasksRouter)
+app.get("/", (req, res) => {
+  res.send("Task Manager");
+});
 
-app.listen(port, () => console.log(`http://localhost:${port}`));
-
-app.get('/', (req, res) => {
-    res.send('Task Manager');
-})
+run();
---
+++ db/connection.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose");
+
+const connectDB = async (uri) => {
+  await mongoose.connect(uri);
+};
+module.exports = connectDB;
