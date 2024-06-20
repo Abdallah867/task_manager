002e4829654d8acbacfc43017bd54907b42ec624
Abdallah867 <abdoallahusma5704@gmail.com>
2024-06-20 20:54:10 +0200

    not found setup

--- app.js
+++ app.js
@@ -2,9 +2,23 @@ const express = require("express");
 const connectDB = require("./db/connection");
 const app = express();
 const tasksRouter = require("./routes/tasks");
+const notFound = require("./middleware/not_found");
 require("dotenv").config();
 
 const port = 5000;
+
+app.use(express.static("./public"));
+
+app.use(express.json());
+
+app.use("/api/v1/tasks", tasksRouter);
+
+app.use(notFound);
+
+app.get("/", (req, res) => {
+  res.send("Task Manager");
+});
+
 const run = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
@@ -13,12 +27,5 @@ const run = async () => {
     console.log(error);
   }
 };
-app.use(express.json());
-
-app.use("/api/v1/tasks", tasksRouter);
-
-app.get("/", (req, res) => {
-  res.send("Task Manager");
-});
 
 run();
